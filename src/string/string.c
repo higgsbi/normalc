3006cb429f3845bc49d5b7083f402c8934f173f5
Higgsbi <contact@xenon.pm>
2024-10-29 16:43:45 -0400

    spacing

--- src/string/string.c
+++ src/string/string.c
@@ -188,7 +188,7 @@ String* string_sub(String* src, size_t start, size_t length) {
 String* string_replace(String *src, char *replaced, char *replacer) {
 	ASSERT_NONNULL(src);
 	ASSERT_NONNULL(replaced);
-	ASSERT_NONNULL(replaced);
+	ASSERT_NONNULL(replacer);
 
 	StringBuilder* builder = string_builder_new();	
 
@@ -216,11 +216,12 @@ String* string_replace(String *src, char *replaced, char *replacer) {
 			string_builder_append(builder, replacer);	
 			i += length_replaced - 1;
 			start_unmatched = i + 1;
-		} else if (i == src->length -1) {
+		} else if (i == src->length - 1) {
 			// 3.) Add unmatched tail from source
 			string_builder_append_substring(builder, src->buffer, start_unmatched, i - start_unmatched);			
 		}
 	}
+
 
 	String* built = string_builder_build(builder);
 	string_builder_free(builder);	
